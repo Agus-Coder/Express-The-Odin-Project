b4afbfbb1cb83505ac2f38a4fc19aa48e0b664c6
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-09-25 17:25:50 -0300

    re-coded nodejs server exercise using express. index links not working

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/pages/about.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "/pages/about.html"));
+});
+
+app.get("/pages/contact-me.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "/pages/contact-me.html"));
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+
