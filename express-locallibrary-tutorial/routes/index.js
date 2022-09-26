b521065e4b2dcce27aebd3d10992129f5caedee4
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-09-26 19:12:12 -0300

    Added Comments

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('cool', { title: 'Express' });
 });
 
 module.exports = router;
+
+// ORM = Object Relational Model
+// ODM = Object Data Model
