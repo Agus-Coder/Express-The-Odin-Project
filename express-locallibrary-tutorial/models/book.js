c843261d8052baca3d029349e93778f2f8a72585
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-10-01 11:26:24 -0300

    Coded book and bookInstance Models from mongoose

---
+++ express-locallibrary-tutorial/models/book.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
+
+//Virtual for book url
+BookSchema.virtual("url").get(function () {
+  return `/catalog/book.${this._id}`;
+});
+
+//Export model
+
+module.exports = mongoose.model("Book", BookSchema);
